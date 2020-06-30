2a143f69bd4dbf69a0acaf8ef74e09a4d58becca
Ashley <ashleykubera@gmail.com>
2020-06-30 12:57:12 -0400

    Created main application class

---
+++ src/main/java/io/github/msdottee/courier/CourierApplication.java
@@ -0,0 +1,12 @@
+package io.github.msdottee.courier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourierApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourierApplication.class, args);
+    }
+}
