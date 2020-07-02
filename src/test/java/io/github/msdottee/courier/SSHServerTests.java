621b6c3f3d1188c2f136137812b394318f28a772
Ashley <ashleykubera@gmail.com>
2020-07-01 20:12:56 -0400

    Fixed issues with JUnit5 and Spring

---
+++ src/test/java/io/github/msdottee/courier/SSHServerTests.java
@@ -0,0 +1,14 @@
+package io.github.msdottee.courier;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+public class SSHServerTests {
+
+    @Test
+    public void empty() {
+
+    }
+}
