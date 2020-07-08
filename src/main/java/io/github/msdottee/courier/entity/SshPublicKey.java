62ffb033d0d6942b0d82edcc959e69fd711f3f87
ashley <ashleykubera@gmail.com>
2020-07-07 21:04:30 -0400

    added field to database table

--- src/main/java/io/github/msdottee/courier/entity/SshPublicKey.java
+++ src/main/java/io/github/msdottee/courier/entity/SshPublicKey.java
@@ -9,13 +9,22 @@ import javax.persistence.Id;
 public class SshPublicKey {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-    public Long id;
+    private Long id;
+    private String key;
 
     public Long getId() {
         return id;
     }
 
+    public String getKey() {
+        return key;
+    }
+
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
     }
 }
