aaed605401349c13fc47f7b60989c45ca828756e
ashley <ashleykubera@gmail.com>
2020-07-15 18:11:42 -0400

    Added S3 dependencies

--- src/main/java/io/github/msdottee/courier/service/SSHServerService.java
+++ src/main/java/io/github/msdottee/courier/service/SSHServerService.java
@@ -1,14 +1,18 @@
 package io.github.msdottee.courier.service;
 
+import org.apache.sshd.common.file.FileSystemFactory;
 import org.apache.sshd.server.SshServer;
 import org.apache.sshd.server.auth.pubkey.PublickeyAuthenticator;
 import org.apache.sshd.server.keyprovider.SimpleGeneratorHostKeyProvider;
+import org.apache.sshd.server.subsystem.SubsystemFactory;
+import org.apache.sshd.server.subsystem.sftp.SftpSubsystemFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
 import java.io.IOException;
+import java.util.Collections;
 
 @Service
 public class SSHServerService {
@@ -19,12 +23,24 @@ public class SSHServerService {
     @Autowired
     private PublicKeyAuthenticatorService publicKeyAuthenticatorService;
 
+    @Autowired
+    private S3FileSystemAccessorService s3FileSystemAccessorService;
+
     @PostConstruct
     public void initializeSshServer() throws IOException {
         SshServer sshd = SshServer.setUpDefaultServer();
+
         sshd.setPort(port);
         sshd.setKeyPairProvider(new SimpleGeneratorHostKeyProvider());
         sshd.setPublickeyAuthenticator(publicKeyAuthenticatorService);
+        sshd.setSubsystemFactories(Collections.singletonList(createFileSystemFactory()));
+
         sshd.start();
+    }
+
+    private SubsystemFactory createFileSystemFactory() {
+        return new SftpSubsystemFactory.Builder()
+                .withFileSystemAccessor(s3FileSystemAccessorService)
+                .build();
     }
 }
